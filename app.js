06d3fa2e3c2f771e84fa0008d67983ec1450510b
Justin Silvestre <justinsilvestre@gmail.com>
2015-07-26 22:08:24 -0400

    added about page, twitter popup

--- app.js
+++ app.js
@@ -1,11 +1,58 @@
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
+var express = require('express'),
+		app = express(),
+		server = require('http').createServer(app),
+		ejs = require('ejs'),
+		OAuth = require('oauth'),
+    moment = require('moment');
 
-app.get('/', function(request, response) {
-	response.sendFile(__dirname + '/index.html')
-});
+var environment = (process.env.NODE_ENV === 'production') ? process.env : require('./twitterstuff');
 
-app.use(express.static('public'))
+
+var oauth = new OAuth.OAuth(
+  'https://api.twitter.com/oauth/request_token',
+  'https://api.twitter.com/oauth/access_token',
+  environment.TWITTER_API_KEY,
+  environment.TWITTER_CONSUMER_SECRET,
+  '1.0',
+  null,
+  'HMAC-SHA1'
+  );
+
+app.renderResponse = function(filename) {
+	return function(request, response) {
+
+  	var twitterUrl = require('url').format({
+      protocol: 'https:',
+      host: 'api.twitter.com',
+      pathname: '/1.1/statuses/user_timeline.json',
+      query: { screen_name: 'justsilvestrein', count: 1 }
+    });
+
+    oauth.get(
+      twitterUrl,
+      environment.TWITTER_APP_TOKEN,
+      environment.TWITTER_APP_SECRET,
+
+      function (event, data, res){
+        if (event) console.error(event);        
+        console.log(data);
+        var tweets = JSON.parse(data);
+        var tweet = tweets[0];
+        tweet.full_text = tweet.retweeted ? tweet.retweeted_status.text : tweet.text;
+
+        var then = moment(tweet.created_at)
+        tweet.tweet_info = moment(then).fromNow();
+
+        response.render(filename, {tweet: tweet}); 
+      }
+    );
+  };
+};
+
+app.get('/', app.renderResponse('index.ejs'));
+
+app.get('/about', app.renderResponse('about.ejs'));
+
+app.use(express.static('public'));
 
 app.listen(process.env.PORT || 8080);
--- public/index.js
+++ public/index.js
@@ -1,15 +1,33 @@
+/*	var scrollBg = function() {
+			var bg, section;
+			for (var i = 1; i <= 3; i++) {
+				bg = $('#bg' + i);
+				section = $('.assets').children('section:nth-of-type('+ i + ')');
+
+				var scrollTop = $(document).scrollTop();
+				var viewportHeight = $(window).height();
+
+				var reachedBg = ( scrollTop + (viewportHeight - bg.outerHeight())/2 ) >= bg.offset().top;
+				var mainContent = $('#main-content');
+				// var reachedBg = ( scrollTop + (viewportHeight - bg.outerHeight())/2 ) >= ( (mainContent.offset().top + mainContent.height()) * (0.25 + i * 0.25) );
+
+				bg.toggleClass('scrolled', reachedBg);
+				// bg.toggleClass('scrolled', ($(document).scrollTop() + ($(window).height()/2) - (bg.height()/2)) >= (bg.offset().top) )
+			}
+	};*/
+
+
 $(function(){
 
-	$('body').removeClass('stop-transitions');
 
 	var assetsClicked = 0;
-	$('.assets').on('click', 'h2', function() {
+	$('#assets').on('click', 'h2', function() {
 		if(!$(this).hasClass('clicked')) assetsClicked += 1;
 		$(this).addClass('clicked');
 
 		var i = ($(this).index() + 1 + 1) / 2;
 		$('.details').removeClass('active');
-		$('.assets').children('section:nth-of-type('+ i + ')').addClass('active');
+		$('#assets').children('section:nth-of-type('+ i + ')').addClass('active');
 
 
 		if (assetsClicked <3) {
@@ -22,7 +40,13 @@ $(function(){
 			$('h1').addClass('all-clicked');
 			//body addclass allclicked.... change styles
 					$('.bg').addClass('active');
-		} 
+		}
+
 	});
+
+
+		// scrollBg();
+		// $(document).scroll(scrollBg);
+
 
 });
