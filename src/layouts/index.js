904fea89b31ff318b3a04a178c15c0c231d9a3a2
Justin Silvestre <justinsilvestre@gmail.com>
2018-03-04 14:04:51 +0100

    Update meta description

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -11,6 +11,9 @@ const TemplateWrapper = ({ children, location }) => (
     <link href="https://fonts.googleapis.com/css?family=Roboto|Open+Sans:300,600|Raleway:400,200|Quattrocento:400,700|Maven+Pro" rel="stylesheet" />
     <Helmet
       title="Justin Silvestre - Web Developer"
+      meta={[
+        { name: 'description', content: 'Berlin-based web developer specializing in functional frontend code.' },
+      ]}
       bodyAttributes={{ id: location.pathname === '/' ? 'home' : 'about' }}
     />
 
