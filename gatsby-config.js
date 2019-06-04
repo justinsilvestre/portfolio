536843dc1f033fc1dad76300a5313a81e24f408a
Justin Silvestre <justinsilvestre@gmail.com>
2019-06-04 23:47:42 +0200

    Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,14 @@ module.exports = {
   siteMetadata: {
     title: 'Justin Silvestre - Software Developer',
   },
-  plugins: ['gatsby-plugin-sass', 'gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-sass',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.googleAnalyticsTrackingId,
+      },
+    },
+  ],
 }
