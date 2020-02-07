f2f7f81e48f2221d0af0eaef5de438ab50b4a57d
Justin Silvestre <justinsilvestre@gmail.com>
2020-02-07 14:24:00 +0100

    Update deps

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,14 +2,5 @@ module.exports = {
   siteMetadata: {
     title: 'Justin Silvestre - Software Developer',
   },
-  plugins: [
-    'gatsby-plugin-sass',
-    'gatsby-plugin-react-helmet',
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID,
-      },
-    },
-  ],
+  plugins: ['gatsby-plugin-sass', 'gatsby-plugin-react-helmet'],
 }
