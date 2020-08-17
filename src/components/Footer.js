1e4809d9fbd4da1837b461101c133caf806407c0
Justin Silvestre <justinsilvestre@gmail.com>
2020-08-17 10:53:53 +0200

    Move footer styles to module

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,48 +1,50 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import css from './Footer.module.css'
+import cn from 'classnames'
 
 const Footer = ({ location: { pathname } }) => {
   const strippedPathname = pathname.replace(/\//g, '')
   return (
-    <footer className={strippedPathname === '' ? css.homeFooter : ''}>
+    <footer className={cn(css.container, { [css.homeFooter]: strippedPathname })}>
       <ul>
         {strippedPathname !== '' && (
-          <li>
-            <Link to="/">home</Link>
+          <li className={css.navLi}>
+            <Link to="/" className={css.link}>home</Link>
           </li>
         )}
         {strippedPathname !== 'about' && (
-          <li>
-            <Link to="/about">about me</Link>
+          <li className={css.navLi}>
+            <Link to="/about" className={css.link}>about me</Link>
           </li>
         )}
-        <li>
-          <a id="twitter" href="http://twitter.com/justsilvestrein">
-            <i class="fa fa-twitter" /> Twitter
+        <li className={css.navLi}>
+          <a className={css.twitterLink} href="http://twitter.com/justsilvestrein">
+            <i className="fa fa-twitter" /> Twitter
           </a>
         </li>
-        <li>
-          <a href="https://github.com/justinsilvestre?tab=repositories">
-            <i class="fa fa-github" /> Github
+        <li className={css.navLi}>
+          <a href="https://github.com/justinsilvestre?tab=repositories" className={css.link}>
+            <i className="fa fa-github" /> Github
           </a>
         </li>
-        <li>
-          <a href="mailto:justinsilvestre@gmail.com">
-            <i class="fa fa-envelope" /> justinsilvestre@gmail.com
+        <li className={css.navLi}>
+          <a href="mailto:justinsilvestre@gmail.com" className={css.link}>
+            <i className="fa fa-envelope" /> justinsilvestre@gmail.com
           </a>
         </li>
-        <li>
+        <li className={css.navLi}>
           <a
             href="https://docs.google.com/document/d/1ZdEm8Bn1Z-61WmZ1pq27CFos7-vgabrVJOiYX8odFcI/edit?usp=sharing"
             target="_blank"
+            className={css.link}
           >
             CV
           </a>
         </li>
         {strippedPathname !== 'imprint' && (
-          <li style={{ opacity: '0.3' }}>
-            <Link to="/imprint">Impressum</Link>
+          <li className={css.navLi} style={{ opacity: '0.3' }}>
+            <Link to="/imprint" className={css.link}>Impressum</Link>
           </li>
         )}
       </ul>
