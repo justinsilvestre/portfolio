a3eb3d916af85ddfe4f90784acfc53cd2c4ae461
Justin Silvestre <justinsilvestre@gmail.com>
2020-08-17 11:20:07 +0200

    Fix footer nav icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,16 @@ import React from 'react'
 import { Link } from 'gatsby'
 import css from './Footer.module.css'
 import cn from 'classnames'
+import Icon_ from 'react-icons-kit'
+import { github } from 'react-icons-kit/icomoon/github'
+import { twitter } from 'react-icons-kit/icomoon/twitter'
+import { mail } from 'react-icons-kit/icomoon/mail'
+
+function Icon({ icon }) {
+  return <span className={css.iconWrapper}>
+    <Icon_ icon={icon} size={20} />
+  </span>
+}
 
 const Footer = ({ location: { pathname } }) => {
   const strippedPathname = pathname.replace(/\//g, '')
@@ -20,17 +30,17 @@ const Footer = ({ location: { pathname } }) => {
         )}
         <li className={css.navLi}>
           <a className={css.twitterLink} href="http://twitter.com/justsilvestrein">
-            <i className="fa fa-twitter" /> Twitter
+            <Icon icon={twitter} /> Twitter
           </a>
         </li>
         <li className={css.navLi}>
           <a href="https://github.com/justinsilvestre?tab=repositories" className={css.link}>
-            <i className="fa fa-github" /> Github
+          <Icon icon={github} /> Github
           </a>
         </li>
         <li className={css.navLi}>
           <a href="mailto:justinsilvestre@gmail.com" className={css.link}>
-            <i className="fa fa-envelope" /> justinsilvestre@gmail.com
+          <Icon icon={mail} /> justinsilvestre@gmail.com
           </a>
         </li>
         <li className={css.navLi}>
