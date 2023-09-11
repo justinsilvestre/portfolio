6817f03070c38637f3229834889757f8a4c6bda8
justinsilvestre <justinsilvestre@gmail.com>
2023-09-11 12:32:43 +0200

    Copy update

--- pages/imprint.tsx
+++ pages/imprint.tsx
@@ -1,30 +1,33 @@
 import React, { Component, Fragment } from 'react'
 import css from './imprint.module.scss'
 import Layout from '../components/MainLayout'
+import { GetStaticProps, InferGetStaticPropsType } from 'next'
 
-const impressumAddress = process.env.NEXT_IMPRESSUM_ADDRESS || ''
-
-console.log({ impressumAddress })
-class Imprint extends Component {
-  render() {
-    return (
-      <Layout>
-        <section className={css.container}>
-          <h1 className={css.heading}>Impressum</h1>
-          <p>justinsilvestre@gmail.com</p>
-          <p>
-            Justin Silvestre
-            {impressumAddress.split(' / ').map((segment) => (
-              <Fragment key={segment}>
-                <br />
-                {segment}
-              </Fragment>
-            ))}
-          </p>
-        </section>
-      </Layout>
-    )
-  }
+export default function Imprint({
+  address,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  return (
+    <Layout>
+      <section className={css.container}>
+        <h1 className={css.heading}>Impressum</h1>
+        <p>justinsilvestre@gmail.com</p>
+        <p>
+          Justin Silvestre
+          {address.split(' / ').map((segment) => (
+            <Fragment key={segment}>
+              <br />
+              {segment}
+            </Fragment>
+          ))}
+        </p>
+      </section>
+    </Layout>
+  )
 }
 
-export default Imprint
+export const getStaticProps: GetStaticProps<{
+  address: string
+}> = async () => {
+  const address = process.env.NEXT_IMPRESSUM_ADDRESS || ''
+  return { props: { address } }
+}
