59b90e541d0e72d6dbfca939008e0b84588ba265
justinsilvestre <justinsilvestre@gmail.com>
2023-09-11 11:24:00 +0200

    Copy update

--- pages/imprint.tsx
+++ pages/imprint.tsx
@@ -1,22 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import css from './imprint.module.scss'
 import Layout from '../components/MainLayout'
 
 class Imprint extends Component {
   render() {
-
     return (
       <Layout>
         <section className={css.container}>
           <h1 className={css.heading}>Impressum</h1>
+          <p>justinsilvestre@gmail.com</p>
           <p>
-            justinsilvestre@gmail.com
-          </p>
-          <p>
-            Justin Silvestre<br />
-            c/o RA Matutis<br />
-            Berliner Straße 57<br />
-            14467 Potsdam<br />
+            Justin Silvestre
+            {process.env.IMPRESSUM_ADDRESS?.split(' / ').map((segment) => (
+              <Fragment key={segment}>
+                <br />
+                {segment}
+              </Fragment>
+            ))}
           </p>
         </section>
       </Layout>
