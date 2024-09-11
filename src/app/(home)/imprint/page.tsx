b94ea3683abc249c8003752fa1f5d0f6350ad8a1
Justin Silvestre <justinsilvestre@gmail.com>
2024-09-11 17:02:20 +0200

    Fix imprint layout

--- src/app/(home)/imprint/page.tsx
+++ src/app/(home)/imprint/page.tsx
@@ -1,25 +1,21 @@
-import React, { Component, Fragment } from 'react'
+import React, { Fragment } from 'react'
 import css from './imprint.module.scss'
-import Layout from '../../../components/MainLayout'
-import { GetStaticProps, InferGetStaticPropsType } from 'next'
 
 export default function Imprint() {
   const address = process.env.GHP_IMPRESSUM_ADDRESS || ''
   return (
-    <Layout>
-      <section className={css.container}>
-        <h1 className={css.heading}>Impressum</h1>
-        <p>justinsilvestre@gmail.com</p>
-        <p>
-          Justin Silvestre
-          {address.split(' / ').map((segment) => (
-            <Fragment key={segment}>
-              <br />
-              {segment}
-            </Fragment>
-          ))}
-        </p>
-      </section>
-    </Layout>
+    <section className={css.container}>
+      <h1 className={css.heading}>Impressum</h1>
+      <p>justinsilvestre@gmail.com</p>
+      <p>
+        Justin Silvestre
+        {address.split(' / ').map((segment) => (
+          <Fragment key={segment}>
+            <br />
+            {segment}
+          </Fragment>
+        ))}
+      </p>
+    </section>
   )
 }
