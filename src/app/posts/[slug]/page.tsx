b4951e22a30739441c81c4db6ea47d8454bc3b1e
Justin Silvestre <justinsilvestre@gmail.com>
2024-09-11 12:46:18 +0200

    Enable blog preview

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -71,5 +71,9 @@ export async function generateMetadata({ params }: Params): Promise<Metadata> {
 }
 
 export async function generateStaticParams() {
-  return []
+  const posts = getAllPosts();
+
+  return posts.map((post) => ({
+    slug: post.slug,
+  }));
 }
