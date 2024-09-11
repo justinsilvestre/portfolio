147efe682dac6fae001b9481c62a403943107aec
Justin Silvestre <justinsilvestre@gmail.com>
2024-09-11 13:07:39 +0200

    Fix header readability on mobile

--- src/app/(home)/index.tsx
+++ src/app/(home)/index.tsx
@@ -7,7 +7,7 @@ function IndexPage() {
 
   return (
     <div className='flex flex-col items-center justify-center md:h-[calc(100vh-3rem)] w-full pt-16 md:pt-0 px-8'>
-      <h1 className='text-4xl z-10'>Justin Silvestre<br /><span className="text-3xl">Software developer</span></h1>
+      <h1 className='text-4xl z-10 max-md:[backdrop-filter:blur(10px)]'>Justin Silvestre<br /><span className="text-3xl">Software developer</span></h1>
       <p className="text-xl mt-5 bg-[#ececec55] [backdrop-filter:blur(10px)] rounded-lg p-2 text-center">
         TypeScript, React, Node, Electron, and&nbsp;more.
       </p>
