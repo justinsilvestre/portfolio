2796929bcf8f4b4a26b8dbb868221bf9d0964bbf
Justin Silvestre <justinsilvestre@gmail.com>
2024-09-12 16:39:09 +0200

    Add links

--- src/app/(home)/about/about.tsx
+++ src/app/(home)/about/about.tsx
@@ -9,7 +9,7 @@ const pimpleGif = '/images/oldgifs/pimplelqsmall.gif'
 export default function About() {
   const [{ flash }, setState] = useState({ flash: false })
   return (
-    <section className={css.container}>
+    <section className={cn(css.container, 'my-0 mx-auto lg:pt-16')}>
       <h1 className={css.header}>About Justin</h1>
 
       <ul className={css.trajectory}>
@@ -21,27 +21,24 @@ export default function About() {
           </span>{' '}
           <span className={css.trajectoryYear}>1991</span>
         </li>{' '}
-        <li className={css.trajectoryLi}>→</li>
+        <li className={css.trajectoryLiArrow}>→</li>
         <li className={css.trajectoryLi}>
           <span className={css.trajectoryLocation}>New York</span>{' '}
           <span className={css.trajectoryYear}>2010</span>
         </li>{' '}
-        <li className={css.trajectoryLi}>→</li>
+        <li className={css.trajectoryLiArrow}>→</li>
         <li className={css.trajectoryLi}>
           <span className={css.trajectoryLocation}>Dallas</span>{' '}
           <span className={css.trajectoryYear}>2014</span>
         </li>{' '}
-        <li className={css.trajectoryLi}>→</li>
+        <li className={css.trajectoryLiArrow}>→</li>
         <li className={css.trajectoryLi}>
           <span className={css.trajectoryLocation}>Berlin</span>{' '}
           <span className={css.trajectoryYear}>2017</span>
         </li>
       </ul>
-
-      <p className={css.p}>Life goal: joining beauty with utility.</p>
-
       <p className={css.p}>
-        Justin's experience reaches back to his childhood, when he made
+        Justin's life goal is to join <span className="font-bold">beauty</span> with <span className="font-bold">utility</span>. His experience writing software reaches back to childhood, when he made
         websites to showcase his artwork and{' '}
         <a
           className={css.flashLink}
@@ -50,8 +47,33 @@ export default function About() {
         >
           experiments in Flash
         </a>
-        . That creative bent has translated to a passion for expressiveness
+        .
+        That creative bent has translated to a passion for expressiveness
         and sound craftsmanship in his code.
+      </p>
+      <p className={css.p}>
+        Here are some things he's been working on recently:
+      </p>
+      <p className={css.p}>
+        <a href="https://knowclip.com"><img src="/images/knowclip-screenshot.png" alt="" className=" shadow-lg shadow-black/65 mb-4" /></a>
+        <a href="https://knowclip.com" className="underline font-bold">Knowclip</a> is a cross-platform desktop app for{' '}
+        creating multimedia flashcards.
+        Justin used it himself to learn Japanese, achieving the top N1 level on the  <a className="underline" href="https://en.wikipedia.org/wiki/Japanese-Language_Proficiency_Test">Japanese Language Proficiency Test</a>
+        {' '}in July 2024.
+      </p>
+      <p className={css.p}>
+        <a href="https://kanjisense.com"><img src="/images/kanjisense-screenshot.png" alt="" className=" shadow-lg shadow-black/65 mb-4" /></a>
+        <a href="https://kanjisense.com" className="underline font-bold">Kanjisense</a> is a{' '}
+        free online dictionary for learners of Japanese characters.
+        It breaks down each of the approximately 3500 <i>kanji</i> characters needed for adult-level literacy in Japanese into their graphic components,
+        {' '}explaining their forms and their origins, while also <span className="font-bold">illuminating the connections</span> between different characters.
+      </p>
+      <p className={css.p}>
+        <a href="https://github.com/justinsilvestre/hanlib"><img src="/images/hanlib-screenshot.png" alt="" className=" shadow-lg shadow-black/65 mb-4" /></a>
+        <a href="https://github.com/justinsilvestre/hanlib" className="underline font-bold">Hanlib</a> is a digital library of classical Chinese texts, powered
+        by a special <i>domain-specific language</i>.
+        For each text, one source code file is used to  <span className="font-bold">automatically generate an interactive Chinese&ndash;English gloss</span> at the same time as an idiomatic English translation.
+        Currently a work in progress.
       </p>
       <div
         className={cn(css.flash, { [css.flashActive]: flash })}
--- src/app/(home)/index.tsx
+++ src/app/(home)/index.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import Link from 'next/link'
 import React from 'react'
 
 
@@ -9,7 +10,7 @@ function IndexPage() {
     <div className='flex flex-col items-center justify-center md:h-[calc(100vh-3rem)] w-full pt-16 md:pt-0 px-8'>
       <h1 className='text-4xl z-10 max-md:[backdrop-filter:blur(10px)]'>Justin Silvestre<br /><span className="text-3xl">Software developer</span></h1>
       <p className="text-xl mt-5 bg-[#ececec55] [backdrop-filter:blur(10px)] rounded-lg p-2 text-center">
-        TypeScript, React, Node, Electron, and&nbsp;more.
+        TypeScript, React, Node, Electron, <Link className='underline' href="/about">and&nbsp;more</Link>.
       </p>
     </div>
   )
