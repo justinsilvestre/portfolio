3382f5184baebc77d75968dbfa49b35f29d3b6b5
Justin Silvestre <justinsilvestre@gmail.com>
2025-05-15 16:33:24 +0900

    Add links to dc home in layout

--- src/app/dc/(projects)/layout.tsx
+++ src/app/dc/(projects)/layout.tsx
@@ -3,6 +3,7 @@ import 'tailwindcss/tailwind.css'
 import { PropsWithChildren } from 'react'
 import type { Metadata } from 'next'
 import { FooterLink } from '../components/FooterLink'
+import Link from 'next/link'
 
 export const metadata: Metadata = {
   title: 'Justin Silvestre | M.A. in Design and Computation, Winter 2025',
@@ -11,8 +12,11 @@ export const metadata: Metadata = {
 export default function DcLayout({ children }: PropsWithChildren) {
   return (
     <div>
-      <header className="text-right max-w-screen-xl my-4 mx-auto px-4 text-sm pt-1 text-gray-500">
-        Justin&nbsp;Silvestre - M.A. in Design and Computation, Winter&nbsp;2025
+      <header className="text-right max-w-screen-xl my-4 mx-auto px-4 text-sm pt-1">
+        <Link href="/dc/" className="text-gray-500 hover:underline">
+          Justin&nbsp;Silvestre - M.A. in Design and Computation,
+          Winter&nbsp;2025
+        </Link>
       </header>
       {children}
       <footer className="clear-left my-8">
@@ -40,8 +44,10 @@ export default function DcLayout({ children }: PropsWithChildren) {
           />
         </ul>
         <p className="m-4 text-gray-500 text-sm text-center">
-          Justin&nbsp;Silvestre - M.A. in Design and Computation,
-          Winter&nbsp;2025
+          <Link href="/dc/" className="text-gray-500 hover:underline">
+            Justin&nbsp;Silvestre - M.A. in Design and Computation,
+            Winter&nbsp;2025
+          </Link>
         </p>
       </footer>
     </div>
